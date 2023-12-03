7bba062bb53b9ef365bfe418d6f597086fcaae25
ammobooy <144812875+ammobooy@users.noreply.github.com>
2023-12-03 19:04:12 +0700

    Fixing DAlle try/exceptt

--- handlers/client/neural/dalle.py
+++ handlers/client/neural/dalle.py
@@ -1,5 +1,11 @@
 import openai
+from openai.error import InvalidRequestError
+import logging as lg
+
+from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, FSInputFile
+
+from filters.states.state import ClientState
 from utils.func import check_ban_words
 from config_data.config import config
 from config_data.create_bot import bot, db
@@ -9,31 +15,37 @@ from utils.func import get_text
 openai.api_key = config['APIToken']
 
 
-async def dalle_image(message: Message):
+async def dalle_image(message: Message, state: FSMContext):
     prompt = message.text
     chat_id = message.from_user.id
     sticker_file = FSInputFile(config["StickerGPT"])
     wait_msg = await message.answer_sticker(sticker=sticker_file)
-    wait_msg_id = wait_msg.message_id
 
     ban_word = check_ban_words(prompt)
     if ban_word:
         await wait_msg.delete()
         return await message.answer(f"{get_text('text.ban_words')} - <code>{ban_word}</code>")
 
-    response = await openai.Image.acreate(
-        model="dall-e-3",
-        prompt=prompt,
-        n=1,
-        size="1024x1024",
-        quality="standard",
-        response_format="url",
-    )
+    await state.set_state(ClientState.process)
 
-    await bot.delete_message(
-        chat_id=chat_id,
-        message_id=wait_msg_id
-    )
+    try:
+        response = await openai.Image.acreate(
+            model="dall-e-3",
+            prompt=prompt,
+            n=1,
+            size="1024x1024",
+            quality="standard",
+            response_format="url",
+        )
+    except InvalidRequestError as e:
+        await wait_msg.delete()
+        await state.set_state()
+        await message.answer(get_text("text.error_gpt"))
+        return lg.error(f"ERROR DALLE - {e}")
+
+    await state.set_state()
+    await wait_msg.delete()
+
     photo = response['data'][0]['url']
     await bot.send_photo(chat_id=message.from_user.id, photo=photo, caption=get_text("text.dalle_after_progress"), parse_mode='html')
     reqs = db.read(chat_id, "requests_dalle") - 1
