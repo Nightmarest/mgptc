e8dee668209680fb9928f5481dbd89661cb8e830
ammobooy <144812875+ammobooy@users.noreply.github.com>
2023-12-12 11:47:28 +0700

    Add files via upload

--- config_data/config.py
+++ config_data/config.py
@@ -2,7 +2,7 @@ from aiogram.utils.markdown import hide_link
 
 
 config = {
-    "APIToken": "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", # OPENAI
+    "APIToken": "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", # OPENAI
     "ZeroscopeToken": "r8_DyAbjQpqxhwxSmj9qKVVpjysDmnSUdT4Bkf2g", #зероскоп токен
     "DeeplToken": "725ea090-4ba9-494c-8c58-8912b78537a9",
     "BotToken": "6062729970:AAG1aK3DE-FxpfP7sMeIivaqcQZIRwaQwYA",  # Токен самого бота 6062729970:AAHxJG2GCp4A4cL5fROgKRnn7OSrkD5wEBQ
@@ -61,6 +61,7 @@ config = {
 
     "AdminList": {831472057, 1100294105, 1063430005, 1219187097, 5184973436},  # Список админов 1100294105
     "DevList": {831472057, 5184973436},
+    "NegativeDefault": '''"(worst quality, low quality, normal quality, lowres, low details, oversaturated, undersaturated, overexposed, underexposed, grayscale, bw, bad photo, bad photography, bad art:1.4), (watermark, signature, text font, username, error, logo, words, letters, digits, autograph, trademark, name:1.2), (blur, blurry, grainy), morbid, ugly, asymmetrical, mutated malformed, mutilated, poorly lit, bad shadow, draft, cropped, out of frame, cut off, censored, jpeg artifacts, out of focus, glitch, duplicate, (airbrushed, cartoon, anime, semi-realistic, cgi, render, blender, digital art, manga, amateur:1.3), (3D ,3D Game, 3D Game Scene, 3D Character:1.1), (bad hands, bad anatomy, bad body, bad face, bad teeth, bad arms, bad legs, deformities:1.3), double people"''',
 
     "VideoUrl": "https://telegra.ph/file/b41185d890b48ec6deb77.mp4",
     "BotNickName": "midjourneychatgpt_bot",  # Никнейм Бота для реферальных ссылок
