cc3252531a01732c0e58244e8c5ac2f6e18b4147
ammobooy <144812875+ammobooy@users.noreply.github.com>
2023-12-19 11:45:23 +0700

    UPLOAD VIDEO START

--- config_data/config.py
+++ config_data/config.py
@@ -63,7 +63,7 @@ config = {
     "DevList": {831472057, 5184973436},
     "NegativeDefault": '''"(worst quality, low quality, normal quality, lowres, low details, oversaturated, undersaturated, overexposed, underexposed, grayscale, bw, bad photo, bad photography, bad art:1.4), (watermark, signature, text font, username, error, logo, words, letters, digits, autograph, trademark, name:1.2), (blur, blurry, grainy), morbid, ugly, asymmetrical, mutated malformed, mutilated, poorly lit, bad shadow, draft, cropped, out of frame, cut off, censored, jpeg artifacts, out of focus, glitch, duplicate, (airbrushed, cartoon, anime, semi-realistic, cgi, render, blender, digital art, manga, amateur:1.3), (3D ,3D Game, 3D Game Scene, 3D Character:1.1), (bad hands, bad anatomy, bad body, bad face, bad teeth, bad arms, bad legs, deformities:1.3), double people"''',
 
-    "VideoUrl": "https://telegra.ph/file/b41185d890b48ec6deb77.mp4",
+    "VideoUrl": "https://telegra.ph/file/c1f080d286d2500c0c347.mp4",
     "BotNickName": "midjourneychatgpt_bot",  # Никнейм Бота для реферальных ссылок
     "BotUrl": "https://t.me/midjourneychatgpt_bot",
     "EducationUrl": "https://telegra.ph/Kak-pisat-promty-dlya-Midjourney-50-krutyh-primerov-09-14",
