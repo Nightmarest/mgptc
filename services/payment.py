289702b5416878230baca8fbbbba99913f4d0c2a
Arthur <git@afedko.ru>
2023-12-30 15:45:04 +0500

    Temp. hotfix

--- services/payment.py
+++ services/payment.py
@@ -128,6 +128,7 @@ class cloudpay_api:
                                 subinfo = await resp.json(content_type = None)
                                 resp.close()
                                 await session.close()
+                            print(response)
                             await buy_handler.accrual_requests(buy_type, chatid, response['Model']['Token'], response['Model']['InvoiceId'], subinfo['Model']['Id'])
 
                         elif pay_list[buy_type]["autosub"] is False:
